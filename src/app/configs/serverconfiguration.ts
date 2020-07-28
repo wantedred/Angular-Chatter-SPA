1c06d77695964c0a372fa0238e78e5140a363219
wantedred <wantedrednight@gmail.com>
2020-07-28 01:43:46 -0500

    Pushing to make sure project is up to date

--- src/app/configs/serverconfiguration.ts
+++ src/app/configs/serverconfiguration.ts
@@ -1,6 +1,6 @@
 export class ServerConfigurations {
 
-    public static readonly PRODUCTION: boolean = false;
+    public static readonly PRODUCTION: boolean = true;
 
     public static readonly PORT: string =  "5000";
 
